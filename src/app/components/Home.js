04c3ed5070748b1edc911d00484b543eebb03b4c
saurabhjj <saurabhjj@gmail.com>
2017-08-25 23:39:43 +0530

    learning react till lesson 5

---
+++ src/app/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class Home extends React.Component {
+    render (){
+        return (
+            <div>
+                <p> In a new Component </p>
+                {2+2}
+            </div>
+        );
+    }
+}
